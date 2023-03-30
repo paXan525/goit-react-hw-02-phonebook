99504b5d51e6cb2de4ee66b79c166ce9d244e02e
Pavlo Maslo <maslo525@gmail.com>
2023-03-30 12:10:09 +0300

    д/з

---
+++ src/components/App.styled.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 1000px;
+  margin-right: auto;
+  margin-left: auto;
+`;
+
+export const Title = styled.h1`
+  margin-bottom: 20px;
+
+  font-weight: 700;
+  font-size: 36px;
+  text-align: center;
+  text-transform: uppercase;
+`;
+
+export const SubTitle = styled.h2`
+  margin-bottom: 20px;
+
+  font-weight: 700;
+  font-size: 32px;
+  text-align: center;
+  text-transform: uppercase;
+`;
